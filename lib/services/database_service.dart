5fa33139a68b44bc833265aed716f85f4ff7baa1
Zuzana Botkova <zuzanabotkova@MacBook-Pro-uzivatela-Zuzana.local>
2026-07-26 22:27:43 +0200

    pripravene na testovanie

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import '../models/material_model.dart';
 import '../models/tool_model.dart';
 import '../models/project_model.dart';
@@ -8,87 +9,102 @@ import '../models/guide_model.dart';
 
 class DatabaseService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
-  final String? uid = FirebaseAuth.instance.currentUser?.uid;
 
-  // --- POUŽÍVATEĽ & PREMIUM ---
-  Stream<DocumentSnapshot> get userData {
-    return _db.collection('users').doc(uid).snapshots();
+  // Dynamický getter pre UID - vždy vráti aktuálne ID prihláseného používateľa
+  String? get uid => FirebaseAuth.instance.currentUser?.uid;
+
+  // Pomocná funkcia pre získanie referencie na dokument používateľa
+  DocumentReference get userDoc {
+    if (uid == null) {
+      debugPrint('VAROVANIE: Pokus o prístup k DB bez prihláseného používateľa!');
+    }
+    return _db.collection('users').doc(uid ?? 'unauthenticated');
   }
 
+  // --- POUŽÍVATEĽ & PREMIUM ---
+  Stream<DocumentSnapshot> get userData => userDoc.snapshots();
+
   Stream<bool> get isPremium {
-    return _db.collection('users').doc(uid).snapshots().map((snap) {
+    return userDoc.snapshots().map((snap) {
       if (!snap.exists) return false;
       return (snap.data() as Map<String, dynamic>)['isPremium'] ?? false;
     });
   }
 
-  // Zmena stavu Premium (volané po úspešnom nákupe)
   Future<void> updatePremiumStatus(bool status) async {
-    await _db.collection('users').doc(uid).update({'isPremium': status});
+    await userDoc.update({'isPremium': status});
   }
 
   // --- MATERIÁL & POMÔCKY ---
   Stream<List<MaterialModel>> get materials {
-    return _db.collection('users').doc(uid).collection('materials')
+    return userDoc.collection('materials')
       .orderBy('updatedAt', descending: true)
       .snapshots()
       .map((snap) => snap.docs.map((doc) => MaterialModel.fromFirestore(doc)).toList());
   }
 
-  Future<void> addMaterial(Map<String, dynamic> data) async => await _db.collection('users').doc(uid).collection('materials').add(data);
-  Future<void> updateMaterial(String id, Map<String, dynamic> data) async => await _db.collection('users').doc(uid).collection('materials').doc(id).update(data);
-  Future<void> deleteMaterial(String id) async => await _db.collection('users').doc(uid).collection('materials').doc(id).delete();
+  Future<void> addMaterial(Map<String, dynamic> data) async {
+    debugPrint('Pridávam materiál do: users/$uid/materials');
+    await userDoc.collection('materials').add(data);
+  }
+  
+  Future<void> updateMaterial(String id, Map<String, dynamic> data) async => await userDoc.collection('materials').doc(id).update(data);
+  Future<void> deleteMaterial(String id) async => await userDoc.collection('materials').doc(id).delete();
 
   Stream<List<ToolModel>> get tools {
-    return _db.collection('users').doc(uid).collection('tools')
+    return userDoc.collection('tools')
       .orderBy('updatedAt', descending: true)
       .snapshots()
       .map((snap) => snap.docs.map((doc) => ToolModel.fromFirestore(doc)).toList());
   }
 
-  Future<void> addTool(Map<String, dynamic> data) async => await _db.collection('users').doc(uid).collection('tools').add(data);
-  Future<void> updateTool(String id, Map<String, dynamic> data) async => await _db.collection('users').doc(uid).collection('tools').doc(id).update(data);
-  Future<void> deleteTool(String id) async => await _db.collection('users').doc(uid).collection('tools').doc(id).delete();
+  Future<void> addTool(Map<String, dynamic> data) async => await userDoc.collection('tools').add(data);
+  Future<void> updateTool(String id, Map<String, dynamic> data) async => await userDoc.collection('tools').doc(id).update(data);
+  Future<void> deleteTool(String id) async => await userDoc.collection('tools').doc(id).delete();
 
   // --- PROJEKTY ---
   Stream<List<ProjectModel>> get projects {
-    return _db.collection('users').doc(uid).collection('projects')
+    return userDoc.collection('projects')
       .orderBy('updatedAt', descending: true)
       .snapshots()
       .map((snap) => snap.docs.map((doc) => ProjectModel.fromFirestore(doc)).toList());
   }
 
   Stream<List<ProjectModel>> get activeProjects {
-    return _db.collection('users').doc(uid).collection('projects')
+    return userDoc.collection('projects')
       .where('status', whereIn: ['Vo výrobe', 'Príprava'])
       .snapshots()
       .map((snap) => snap.docs.map((doc) => ProjectModel.fromFirestore(doc)).toList());
   }
 
-  Future<void> addProject(Map<String, dynamic> data) async => await _db.collection('users').doc(uid).collection('projects').add(data);
-  Future<void> updateProject(String id, Map<String, dynamic> data) async => await _db.collection('users').doc(uid).collection('projects').doc(id).update(data);
-  Future<void> deleteProject(String id) async => await _db.collection('users').doc(uid).collection('projects').doc(id).delete();
+  Future<void> addProject(Map<String, dynamic> data) async {
+    debugPrint('Pridávam projekt do: users/$uid/projects');
+    await userDoc.collection('projects').add(data);
+  }
+  
+  Future<void> updateProject(String id, Map<String, dynamic> data) async => await userDoc.collection('projects').doc(id).update(data);
+  Future<void> deleteProject(String id) async => await userDoc.collection('projects').doc(id).delete();
 
   // --- NÁVODY ---
   Stream<List<GuideModel>> get guides {
-    return _db.collection('users').doc(uid).collection('guides')
+    return userDoc.collection('guides')
       .orderBy('updatedAt', descending: true)
       .snapshots()
       .map((snap) => snap.docs.map((doc) => GuideModel.fromFirestore(doc)).toList());
   }
 
-  Future<void> addGuide(Map<String, dynamic> data) async => await _db.collection('users').doc(uid).collection('guides').add(data);
-  Future<void> updateGuide(String id, Map<String, dynamic> data) async => await _db.collection('users').doc(uid).collection('guides').doc(id).update(data);
-  Future<void> deleteGuide(String id) async => await _db.collection('users').doc(uid).collection('guides').doc(id).delete();
+  Future<void> addGuide(Map<String, dynamic> data) async => await userDoc.collection('guides').add(data);
+  Future<void> updateGuide(String id, Map<String, dynamic> data) async => await userDoc.collection('guides').doc(id).update(data);
+  Future<void> deleteGuide(String id) async => await userDoc.collection('guides').doc(id).delete();
 
   // --- UDALOSTI ---
   Stream<List<EventModel>> get events {
-    return _db.collection('users').doc(uid).collection('events')
+    return userDoc.collection('events')
       .snapshots()
       .map((snap) => snap.docs.map((doc) => EventModel.fromFirestore(doc)).toList());
   }
 
-  Future<void> addEvent(Map<String, dynamic> data) async => await _db.collection('users').doc(uid).collection('events').add(data);
-  Future<void> updateEvent(String id, Map<String, dynamic> data) async => await _db.collection('users').doc(uid).collection('events').doc(id).update(data);
-  Future<void> deleteEvent(String id) async => await _db.collection('users').doc(uid).collection('events').doc(id).delete();
+  Future<void> addEvent(Map<String, dynamic> data) async => await userDoc.collection('events').add(data);
+  Future<void> updateEvent(String id, Map<String, dynamic> data) async => await userDoc.collection('events').doc(id).update(data);
+  Future<void> deleteEvent(String id) async => await userDoc.collection('events').doc(id).delete();
 }
